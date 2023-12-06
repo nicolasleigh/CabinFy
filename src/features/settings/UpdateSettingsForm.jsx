f3bccc864580f1089caff53a83d81e5bb36c27be
NicolasLi <linze201@gmail.com>
2023-12-06 16:45:31 +0800

    init

---
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -0,0 +1,76 @@
+import { updateSetting } from '../../services/apiSettings';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
+import Spinner from '../../ui/Spinner';
+import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSetting';
+
+function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    // console.log([field]);
+    // console.log(value);
+    // console.log({ [field]: value });
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
+  // This time we are using UNCONTROLLED fields, so we will NOT store state
+  return (
+    <Form>
+      <FormRow label='Minimum nights/booking'>
+        <Input
+          type='number'
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e, 'minBookingLength')}
+          id='min-nights'
+          disabled={isUpdating}
+        />
+      </FormRow>
+      <FormRow label='Maximum nights/booking'>
+        <Input
+          type='number'
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, 'maxBookingLength')}
+          disabled={isUpdating}
+          id='max-nights'
+        />
+      </FormRow>
+      <FormRow label='Maximum guests/booking'>
+        <Input
+          type='number'
+          defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')}
+          disabled={isUpdating}
+          id='max-guests'
+        />
+      </FormRow>
+      <FormRow label='Breakfast price'>
+        <Input
+          type='number'
+          defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, 'breakfastPrice')}
+          disabled={isUpdating}
+          id='breakfast-price'
+        />
+      </FormRow>
+    </Form>
+  );
+}
+
+export default UpdateSettingsForm;
---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
+import Button from '../ui/Button';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
+import AddCabin from '../features/cabins/AddCabin';
+import CabinTableOperations from '../features/cabins/CabinTableOperations';
+
+function Cabins() {
+  return (
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <CabinTableOperations />
+      </Row>
+
+      <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
+    </>
+  );
+}
+
+export default Cabins;
