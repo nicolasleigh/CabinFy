0c5a1758e9f627ba68aed5ef4f3422fae19b5545
nicolas <linze201@gmail.com>
2024-02-02 10:28:58 +0800

    add typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.js';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './ui/ErrorFallback.jsx';
 import { StyleSheetManager } from 'styled-components';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StyleSheetManager
       shouldForwardProp={() => true}
