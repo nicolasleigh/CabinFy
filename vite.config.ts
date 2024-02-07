80ae21cf94e6b69c7516c8c2168fe6492de3440a
nicolas <linze201@gmail.com>
2024-02-07 16:54:39 +0800

    add vite gzip plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   server: {
     host: true,
     port: 8081, // This is the port which we will use in docker
-    // hmr: {
-    //   clientPort: 443,
-    // },
   },
   optimizeDeps: {
     exclude: ['js-big-decimal'],
