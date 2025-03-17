abf1acaa1c05645fff3ba5c8adcfdd460026845f
Nicolas Leigh <linze201@gmail.com>
2025-03-17 16:08:27 +0800

    fix: fix update setting form error: rendered more hooks than during the previous render

--- frontend/src/features/settings/UpdateSettingsForm.tsx
+++ frontend/src/features/settings/UpdateSettingsForm.tsx
@@ -1,4 +1,4 @@
-import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
 import { useUpdateSetting } from "./useUpdateSetting";
@@ -8,10 +8,9 @@ import { useForm } from "react-hook-form";
 function UpdateSettingsForm() {
   const { isLoading, settings: { minBookingLength, maxBookingLength, maxGuestsPerBooking, breakfastPrice } = {} } =
     useSettings();
+  const form = useForm();
 
   const { isUpdating, updateSetting } = useUpdateSetting();
-
-  if (isLoading) return <Spinner />;
 
   function handleUpdate(e, field) {
     const { value } = e.target;
@@ -19,75 +18,89 @@ function UpdateSettingsForm() {
     updateSetting({ [field]: Number(value) });
   }
 
-  const form = useForm();
+  if (isLoading) return <Spinner />;
 
   // This time we are using UNCONTROLLED fields, so we will NOT store state
   return (
     <Form {...form}>
-      <FormField
-        name='min-nights'
-        render={() => (
-          <FormItem>
-            <FormLabel>Minimum nights</FormLabel>
-            <Input
-              type='number'
-              id='min-nights'
-              disabled={isUpdating}
-              defaultValue={minBookingLength}
-              onBlur={(e) => handleUpdate(e, "minBookingLength")}
-              min={0}
-            />
-          </FormItem>
-        )}
-      />
-      <FormField
-        name='max-nights'
-        render={() => (
-          <FormItem>
-            <FormLabel>Maximum nights</FormLabel>
-            <Input
-              type='number'
-              id='max-nights'
-              defaultValue={maxBookingLength}
-              onBlur={(e) => handleUpdate(e, "maxBookingLength")}
-              disabled={isUpdating}
-              min={0}
-            />
-          </FormItem>
-        )}
-      />
-      <FormField
-        name='max-nights'
-        render={() => (
-          <FormItem>
-            <FormLabel>Maximum guests</FormLabel>
-            <Input
-              type='number'
-              defaultValue={maxGuestsPerBooking}
-              onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
-              disabled={isUpdating}
-              id='max-guests'
-              min={0}
-            />
-          </FormItem>
-        )}
-      />
-      <FormField
-        name='max-nights'
-        render={() => (
-          <FormItem>
-            <FormLabel>Breakfast price</FormLabel>
-            <Input
-              type='number'
-              defaultValue={breakfastPrice}
-              onBlur={(e) => handleUpdate(e, "breakfastPrice")}
-              disabled={isUpdating}
-              id='breakfast-price'
-              min={0}
-            />
-          </FormItem>
-        )}
-      />
+      <form>
+        <FormField
+          name='min-nights'
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel>Minimum nights</FormLabel>
+              <FormControl>
+                <Input
+                  type='number'
+                  id='min-nights'
+                  disabled={isUpdating}
+                  defaultValue={minBookingLength}
+                  onBlur={(e) => handleUpdate(e, "minBookingLength")}
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name='max-nights'
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel>Maximum nights</FormLabel>
+              <FormControl>
+                <Input
+                  type='number'
+                  id='max-nights'
+                  defaultValue={maxBookingLength}
+                  onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+                  disabled={isUpdating}
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name='max-nights'
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel>Maximum guests</FormLabel>
+              <FormControl>
+                <Input
+                  type='number'
+                  defaultValue={maxGuestsPerBooking}
+                  onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
+                  disabled={isUpdating}
+                  id='max-guests'
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name='max-nights'
+          control={form.control}
+          render={() => (
+            <FormItem>
+              <FormLabel>Breakfast price</FormLabel>
+              <FormControl>
+                <Input
+                  type='number'
+                  defaultValue={breakfastPrice}
+                  onBlur={(e) => handleUpdate(e, "breakfastPrice")}
+                  disabled={isUpdating}
+                  id='breakfast-price'
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
     </Form>
   );
 }
